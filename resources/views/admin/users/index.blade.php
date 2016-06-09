59e97650df8185ac9409b493483cafcf01fbeee3
Dare Samuel <dawakesam@gmail.com>
2016-06-09 15:39:50 +0100

    Caret and UserName Fixed

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -32,9 +32,8 @@
 
                    <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
                  <td>{{$user->email}}</td>
-
-                   <td>{{$user->role->name}}</td>
-                 {{--<td>{{$user->role->name}}</td>--}}
+                 <td>{{$user->role ? $user->role->name : "User has no role"}}</td>
+                   {{--<td>{{$user->role->name}}</td>--}}
                  <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
                  <td>{{$user->created_at->diffForHumans()}}</td>
                  <td>{{$user->updated_at->diffForHumans()}}</td>
